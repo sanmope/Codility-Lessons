703ffbd7760bb8f3cf5de0ad3417f44974f8d77d
Santiago Lopez <sanmope@gmail.com>
2016-06-19 11:50:08 -0300

    first atempt of solution

--- Codility-Lessons/src/Main.java
+++ Codility-Lessons/src/Main.java
@@ -1,6 +1,12 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Solution solution = new Solution();
+        solution.arraySol();
+        System.out.println("Array of length: " + solution.arraySol().length);
+        
+        for (int pos: solution.arraySol()){
+            System.out.println("Possition: " + pos);
+        }
     }
 }
---
+++ Codility-Lessons/src/Solution.java
@@ -0,0 +1,12 @@
+/**
+ * Created by santiago on 19/06/16.
+ */
+public class Solution {
+    public int[] arraySol(){
+        int[] A = new int[5];
+        A[0]=3;
+        A[1]=4;
+        return A;
+        
+    }    
+}
